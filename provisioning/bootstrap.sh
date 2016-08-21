53ae81e28da4deb89c3034f470d82b17b1810aa2
tentacode <contact@gabrielpillet.com>
2016-08-21 18:05:48 +0200

    Fixing missing provisionning and app

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -3,9 +3,10 @@ echo "cd /vagrant" >> /home/vagrant/.bashrc
 echo "deb http://packages.dotdeb.org jessie all" > /etc/apt/sources.list.d/dotdeb.list
 wget -O- https://www.dotdeb.org/dotdeb.gpg | apt-key add -
 apt update
+apt-get install -y git
 
 # PHP
-apt-get install -y php7.0 php7.0-curl
+apt-get install -y php7.0 php7.0-curl php7.0-xml
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php composer-setup.php --filename=composer --install-dir=/bin
 php -r "unlink('composer-setup.php');"
